9ea30e306089ebcba7668218d5b9ca2924e8dcf7
Ashish Awasthi <ashish.awasthi2@harman.com>
2020-02-09 19:30:18 +0530

    #09 clear locat reference

--- Pods/Target Support Files/Pods-iOSHTTPConnect-iOSHTTPConnectTests/Pods-iOSHTTPConnect-iOSHTTPConnectTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_iOSHTTPConnect_iOSHTTPConnectTests : NSObject
-@end
-@implementation PodsDummy_Pods_iOSHTTPConnect_iOSHTTPConnectTests
-@end
--- Pods/Target Support Files/iOSReachability/iOSReachability-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_iOSReachability : NSObject
-@end
-@implementation PodsDummy_iOSReachability
-@end
